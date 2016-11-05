22fcac58812717e1f0fd497af1e8dc9e31110a7e
amadeus-tentatio <martinraila@mail.ru>
2016-11-05 12:19:19 +0300

    Fixed main algorithm

--- src/IntegralsSolver.java
+++ src/IntegralsSolver.java
@@ -1,12 +1,18 @@
 public class IntegralsSolver {
 
     public static OutputData integrate(InputData inputData) {
-        int a = inputData.getLowerLimit();
-        int b = inputData.getUpperLimit();
+        int a = inputData.getLowerLimit(), b = inputData.getUpperLimit();
         double precision = inputData.getPrecision();
-        double h, iterationResult = 0, sum, previousIterationResult, newPrecision;
-        int iterationsCounter = 1;
-        h = Math.abs(b - a);
+        int iterationsCounter = 1, temp;
+        double h, iterationResult = 0, sum, previousIterationResult, observationalError;
+        boolean limitsSwapped = false;
+        if (a > b) {
+            temp = a;
+            a = b;
+            b = temp;
+            limitsSwapped = true;
+        }
+        h = b - a;
         do {
             previousIterationResult = iterationResult;
             sum = 0;
@@ -14,24 +20,32 @@ public class IntegralsSolver {
                 sum += getResultOfFunction(inputData, a + i * h);
             }
             iterationResult = h * ((getResultOfFunction(inputData, a) + getResultOfFunction(inputData, b)) * 0.5 + sum);
-            newPrecision = Math.abs(iterationResult - previousIterationResult) / 3;
-            if (newPrecision > precision) {
+            observationalError = Math.abs(iterationResult - previousIterationResult) / 3;
+            if (observationalError > precision) {
                 h *= 0.5;
                 iterationsCounter *= 2;
             }
         }
-        while (newPrecision > precision && iterationsCounter < Math.pow(2, 24)); // 2^24 - maximum number of iterations
-        return new OutputData(iterationResult, iterationsCounter, newPrecision);
+        while (observationalError > precision && iterationsCounter < Math.pow(2, 25)); // 2^25 - optimal number of iterations
+        iterationResult *= limitsSwapped ? -1 : 1;
+        return new OutputData(iterationResult, iterationsCounter, observationalError);
     }
+
     /**
      * test method only
-     */
+     *
     public static void main(String[] args) {
-        ResultsPrinter.printOutputData(IntegralsSolver.integrate(new InputData(Functions.function1, 0, 15, 1)));
-        ResultsPrinter.printOutputData(IntegralsSolver.integrate(new InputData(Functions.function2, 0, 3, 1)));
-        ResultsPrinter.printOutputData(IntegralsSolver.integrate(new InputData(Functions.function3, 1, 16, 0.12)));
+     // test 1 == test1 * (-1) [OK]
+     ResultsPrinter.printOutputData(IntegralsSolver.integrate(new InputData(Functions.function1, 15, 0, 0.123)));
+     // test 2: 1.806314..., 0..15, 0.123, n = 166 [0K]
+     ResultsPrinter.printOutputData(IntegralsSolver.integrate(new InputData(Functions.function1, 0, 15, 0.123)));
+     // test 3: 4443416, 0..8, 0.1, n = 512 [0K]
+     ResultsPrinter.printOutputData(IntegralsSolver.integrate(new InputData(Functions.function2, 0, 8, 0.1)));
+     // test 4: -990, 1..16, 0.1, n = 512 [0K]
+     ResultsPrinter.printOutputData(IntegralsSolver.integrate(new InputData(Functions.function3, 1, 16, 0.1)));
     }
-    /**
+
+     *
      * end of test method
      */
     private static double getResultOfFunction(InputData inputData, double x) {
